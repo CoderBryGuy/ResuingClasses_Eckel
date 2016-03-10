facb699eb4933b903c16458ce4255d4700cc0bb7
CoderBryGuy <bryan.dov.bergman@gmail.com>
2016-03-10 12:46:21 +0200

    Initial commit

---
+++ src/com/company/Ex1.java
@@ -0,0 +1,15 @@
+package com.company;
+
+/**
+ * Created by Bryan on 3/5/2016.
+ */
+public class Ex1 {
+    private int x;
+    public String s;
+    Ex1(int x){
+        this.x = x;
+    }
+    public String toString(){
+        return s;
+    }
+}
---
+++ src/com/company/Main.java
@@ -0,0 +1,11 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        Ex1 x = new Ex1(5);
+        x.s = "hello world";
+        System.out.println(x);
+    }
+}
